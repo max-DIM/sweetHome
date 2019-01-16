316e58c4e5c8cc99cc5d3824fd564e63cc44fa22
fb <a>
2019-01-16 15:32:18 +0100

    test command bin/console make:crud

---
+++ src/Form/AssetType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Asset;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AssetType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Descript')
+            ->add('dailyRate')
+            ->add('nbPerson')
+            ->add('size')
+            ->add('floor')
+            ->add('accomodationType')
+            ->add('state')
+            ->add('street')
+            ->add('zipCode')
+            ->add('gps')
+            ->add('actor')
+            ->add('conditionAsset')
+            ->add('availibilityCalendar')
+            ->add('equipment')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Asset::class,
+        ]);
+    }
+}
