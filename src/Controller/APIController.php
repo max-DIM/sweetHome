a060c29d24d73e0b105fa41cc0b1c0f53f697cea
Digital <digital@c-23.local>
2019-01-17 17:23:09 +0100

    add readme

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -6,7 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
+use App\Entity\Actor;
+use App\Form\ActorType;
+use App\Repository\ActorRepository;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class APIController extends AbstractController
