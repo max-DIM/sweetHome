becc6f7d72cd13fdddcdb8d525b5a7db84bd1013
fb <a>
2019-01-17 11:40:18 +0100

    css

--- public/main.js
+++ public/main.js
@@ -1,4 +1,9 @@
 
 document.addEventListener('DOMContentLoaded',function(){
 
+    /*if (window.location.toString().includes("default")) {
+        var containerImages = document.querySelector('body > div.jumbotron.text-center');
+        containerImages
+    }*/
+
 })
