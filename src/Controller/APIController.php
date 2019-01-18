1e8b3616f700e04b6cf81b3d479873e3b9f80f47
Digital <digital@c-23.local>
2019-01-18 15:19:06 +0100

    api end of dev

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -6,27 +6,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use App\Entity\Actor;
-use App\Form\ActorType;
-use App\Repository\ActorRepository;
+use App\Repository\AssetRepository;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 
 
 class APIController extends AbstractController
 {
     /**
-     * @Route("/a/p/i", name="a_p_i")
+     * @Route("/api/assets", name="api",methods={"GET"})
      */
-    public function index(ActorRepository $assetRepository)
+    public function index(AssetRepository $assetRepository)
     {
+
+
         $assets = $assetRepository->findAll();
+
+        $array = array();
+
+        foreach ($assets as $asset) {
+
+            $array[] = [
+                "descr" => $asset->getDescript(),
+                "state" => $asset->getState(),
+            ];
+        }
+
+        return new JsonResponse($array);
+
+
+        /*
+        //avec jms_serializer
+        $assets = $assetRepository->findAll();
+
+
         $data = $this->get('jms_serializer')->serialize($assets, 'json');
 
         $response = new Response($data);
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;
-
+        */
 
         /*return $this->render('api/index.html.twig', [
             'controller_name' => 'APIController',
